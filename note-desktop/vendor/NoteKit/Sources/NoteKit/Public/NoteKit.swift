a4682520f78606d05082b24b5de2fb063b4b86d6
Adelodunpeter25 <adelodunpeter69@gmail.com>
2026-06-04 21:26:33 +0100

    Implement TodoAttachment, custom NoteLayoutFragment, and corresponding unit tests

--- note-desktop/vendor/NoteKit/Sources/NoteKit/Public/NoteKit.swift
+++ note-desktop/vendor/NoteKit/Sources/NoteKit/Public/NoteKit.swift
@@ -2,4 +2,9 @@ import Foundation
 
 public struct NoteKit {
     public static let version = "1.0.0"
+    
+    /// Initializes NoteKit and registers custom TextKit 2 attachments.
+    public static func initialize() {
+        TodoAttachment.register()
+    }
 }
