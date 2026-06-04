1aaab7eb7fea3c2b3af4efcd7b1c427dc60fa379
Adelodunpeter25 <adelodunpeter69@gmail.com>
2026-06-04 11:33:51 +0100

    Create CustomSearchBar reusable widget

---
+++ lib/widgets/search_bar.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import '../theme.dart';
+
+class CustomSearchBar extends StatelessWidget {
+  final TextEditingController? controller;
+  final String placeholder;
+  final ValueChanged<String>? onChanged;
+
+  const CustomSearchBar({
+    super.key,
+    this.controller,
+    required this.placeholder,
+    this.onChanged,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoSearchTextField(
+      controller: controller,
+      placeholder: placeholder,
+      style: TextStyle(
+        color: AppTextColors.primary(context),
+        fontSize: 16,
+      ),
+      placeholderStyle: TextStyle(
+        color: AppTextColors.quaternary(context),
+        fontSize: 16,
+      ),
+      backgroundColor: AppSurfaces.elevated(context),
+      onChanged: onChanged,
+    );
+  }
+}
