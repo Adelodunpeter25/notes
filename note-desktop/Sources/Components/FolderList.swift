8d6a7eb979bb1831cc147e6f80614f4334b155e9
Adelodunpeter25 <adelodunpeter69@gmail.com>
2026-06-10 13:23:41 +0100

    Restore folder right-click context menu and NSMenuDelegate setup

--- note-desktop/Sources/Components/FolderList.swift
+++ note-desktop/Sources/Components/FolderList.swift
@@ -45,6 +45,8 @@ public final class FolderList: NSViewController, NSOutlineViewDataSource, NSOutl
     private func setupBindings() {
         listView.outlineView.dataSource = self
         listView.outlineView.delegate = self
+        listView.outlineView.menu = FolderContextMenu()
+        listView.outlineView.menu?.delegate = self
         listView.newFolderButton.target = self
         listView.newFolderButton.action = #selector(newFolderButtonTapped)
     }
@@ -151,5 +153,19 @@ public final class FolderList: NSViewController, NSOutlineViewDataSource, NSOutl
         case .trash: onSelectionChanged?(.trash)
         default: break
         }
+    }
+}
+
+extension FolderList: NSMenuDelegate {
+    public func menuNeedsUpdate(_ menu: NSMenu) {
+        let row = listView.outlineView.clickedRow
+        guard let ctxMenu = menu as? FolderContextMenu else { return }
+        
+        guard row != -1, let node = listView.outlineView.item(atRow: row) as? SidebarNode, node.type == .folder else {
+            ctxMenu.removeAllItems(); return
+        }
+        
+        listView.outlineView.selectRowIndexes(IndexSet(integer: row), byExtendingSelection: false)
+        ctxMenu.update(target: self, renameAction: #selector(contextRenameTapped), deleteAction: #selector(contextDeleteTapped))
     }
 }
